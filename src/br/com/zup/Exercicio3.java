5fb4c152b5e549744de2dd9d1a5741a8e28cdda6
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-13 16:39:52 -0300

    Adicionei funcionalidade de exibir o cardapio e excluir pratos

--- src/br/com/zup/Exercicio3.java
+++ src/br/com/zup/Exercicio3.java
@@ -11,11 +11,13 @@ public class Exercicio3 {
         Scanner leitor = new Scanner(System.in);
         Map<String, String> cardapio = new HashMap<String,String>();
 
-        String nomeDoPrato;
-        String ingredientesDoPrato;
+        String nomeDoPrato = "";
+        String ingredientesDoPrato = "";
         boolean exibirMenu = true;
         String exemplo;
         int opcaoMenu = 0;
+        String excluirPrato;
+        String excluir;
 
          while (exibirMenu){
              System.out.println("------ Restaurante MataFome ------");
@@ -25,6 +27,7 @@ public class Exercicio3 {
              System.out.println("3- Excluir prato do cardápio?");
              System.out.println("4- Sair.");
              opcaoMenu = leitor.nextInt();
+             leitor.nextLine();
 
              if (opcaoMenu == 1){
                  System.out.println("Cadastrar novos pratos");
@@ -37,12 +40,27 @@ public class Exercicio3 {
 
 
              }else if(opcaoMenu == 2){
-
+                for (String exibircardapio:cardapio.keySet()){
+                    System.out.println(exibircardapio + "\t || Ingredientes: " + cardapio.get(exibircardapio));
+                }
 
              }else if (opcaoMenu == 3){
 
-             }else if (opcaoMenu == 4){
+                 System.out.println("Digite o nome do prato que deseja excluir: ");
+                 excluirPrato = leitor.nextLine();
 
+                 for (String excluirPratoCardapio:cardapio.keySet()){
+                     if (excluirPratoCardapio.equals(excluirPrato)){
+                         excluir = excluirPrato;
+                     }
+                 }
+
+                 cardapio.remove(excluirPrato);
+
+
+             }else if (opcaoMenu == 4){
+                exibirMenu = false;
+                 System.out.println("Até a proxima!");
              }else {
                  System.out.println("Opção selecionada inválida!");
              }
