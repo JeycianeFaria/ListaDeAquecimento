a5d926a5f7d675a1b45cd78bdfaf6220a9f2ead1
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-13 11:12:08 -0300

    Primeiro commit

---
+++ src/br/com/zup/Exercicio1.java
@@ -0,0 +1,16 @@
+package br.com.zup;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+        Map<String,Double> listaDeProdutos = new HashMap<String,Double>();
+
+    }
+
+}
